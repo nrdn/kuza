d1692a18aaa05f4ca6c9798ef7eebe5fd7e48793
ponyspy <desade4me@gmail.com>
2014-07-23 18:02:17 +0400

    maket

--- public/scripts/project.js
+++ public/scripts/project.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	$('.project_image_main').mapNavigate({offsetX: 0.3, offsetY: 0.3});
+	$('.project_image_main').mapNavigate({offsetX: 0.3333, offsetY: 0.3333});
+
+	$(document).scroll(function(event) {
+		var scroll = 500 - $(document).scrollTop() * 0.6666666;
+
+		$('.project_image_main').css('height', scroll + 'px');
+	});
 });
