4f713ece06436661a44bd6d8c9f516d08ae97978
ponyspy <desade4me@gmail.com>
2014-07-24 20:48:41 +0400

    project

--- public/scripts/project.js
+++ public/scripts/project.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
 	$('.project_image_main').mapNavigate({offsetX: 0.3333, offsetY: 0.3333});
 
 	$(document).scroll(function(event) {
-		var scroll = 500 - $(document).scrollTop() * 0.6666666;
+		var scroll = $(document).scrollTop();
+		var offset = 500 - scroll * 0.666666;
 
-		$('.project_image_main').css('height', scroll + 'px');
+		if (scroll > 600) return false;
+
+		$('.project_image_main').css('height', offset + 'px');
+		$('.project_content_block').css('margin-top', offset + 'px');
 	});
 });
